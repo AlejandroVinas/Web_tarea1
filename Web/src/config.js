e01ca5c35d0f9b23f86788cbe8ac6e50f4ce977d
Alejandro Viñas Portilla <114479197+AlejandroVinas@users.noreply.github.com>
2025-11-08 16:31:33 +0100

    Delete Web/src directory

--- Web/src/config.js
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const config = {
-  port: process.env.PORT || 3000,
-  mongoURI: process.env.MONGO_URI || "mongodb://localhost:27017/portalProductos",
-  jwtSecret: process.env.JWT_SECRET || "claveSuperSecreta"
-};
--- Web/src/middleware/isAdmin.js
+++
@@ -1,5 +0,0 @@
-export function isAdmin(req, res, next) {
-  if (!req.user) return res.status(401).json({ message: "No autenticado" });
-  if (req.user.role !== "admin") return res.status(403).json({ message: "Se requiere rol admin" });
-  next();
-}
